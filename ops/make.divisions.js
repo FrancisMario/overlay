27fe8d6ed4cbbc880b44804df63d7da5e8cc44a1
Marxlo <FRANCISMARIOGOMEZ@GMAIL.COM>
2024-02-12 17:28:56 +0000

    sync

---
+++ ops/make.divisions.js
@@ -0,0 +1,29 @@
+import * as h3 from 'h3-js';
+
+/**
+ * Divide globe into 100 top level hexes, then 
+ * 
+ */
+
+// h3.getNumCells(15)
+// 569707381193162
+
+
+// Home
+// h3.latLngToCell(13.43515481428235, -16.683994666848033, 15)
+// '8f54a9af2873b08', '8e54a9af2873b0f'
+
+// h3.cellArea('8f54a9af2873b08')
+
+// h3.getHexagonAreaAvg(15, 'm2') 
+
+/**
+ * {
+  m: 'm',
+  m2: 'm2',
+  km: 'km',
+  km2: 'km2',
+  rads: 'rads',
+  rads2: 'rads2'
+}
+ */
